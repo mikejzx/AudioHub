d2844456ae0d171192a5a42c4a2ecb1ef307817c
Michael <mikejzx03@gmail.com>
2019-04-20 20:57:13 +1000

    Lot's of slider changes. Package name and urls changed according to the recent repo migration.

--- javascript/sliders.js
+++ javascript/sliders.js
@@ -1,13 +1,34 @@
 
 function slidersInitialise() {
-    document.getElementById("slider-volume-ctrl").oninput = () => {
-        var x = this.value / 100.0;
+    var sliderVol = document.getElementById("slider-volume-ctrl");
+    sliderVol.oninput = () => {
+        var t0 = document.getElementById("slider-volume-ctrl");
+        var x = t0.value / 100.0;
+
         // TODO: Allow user to select a volume 
         // scale curve (linear, logarithmic)
         audio.volume = x * x;
-    }
 
-    document.getElementById("slider-playback-ctrl").oninput = () => {
+        // Move fill
+        var y = t0.value + "% !important;"
+        var t1 = document.getElementById("slider-volume");
+        t1.getElementsByClassName("slider-fill")[0].setAttribute("style", "width: " + y);
+        
+        // Move thumb
+        t1.getElementsByClassName("slider-thumb")[0].setAttribute("style", "left: " + y);
+    };
+    // Force update the initial width.
+    sliderVol.oninput();
+
+    var sliderPlayb = document.getElementById("slider-playback-ctrl");
+    sliderPlayb.oninput = () => {
+        var t0 = document.getElementById("slider-playback-ctrl");
+        var t1 = document.getElementById("slider-playback");
+        var y = t0.value + "% !important;"
+        t1.getElementsByClassName("slider-fill")[0].setAttribute("style", "width: " + y);
+        t1.getElementsByClassName("slider-thumb")[0].setAttribute("style", "left: " + y);
+
         // TODO: Change playback pos based on this.
-    }
+    };
+    sliderPlayb.oninput();
 }
