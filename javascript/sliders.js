ea8140aa6b3abf563eda50f0bb6489beb278486f
Michael <mikejzx03@gmail.com>
2019-04-20 13:21:20 +1000

    Tweaks, playback slider bar started.

--- javascript/sliders.js
+++ javascript/sliders.js
@@ -1,10 +1,13 @@
 
 function slidersInitialise() {
-    var sliderVolume = document.getElementById("slider-volume-ctrl");
-    sliderVolume.oninput = function () {
+    document.getElementById("slider-volume-ctrl").oninput = () => {
         var x = this.value / 100.0;
         // TODO: Allow user to select a volume 
         // scale curve (linear, logarithmic)
         audio.volume = x * x;
+    }
+
+    document.getElementById("slider-playback-ctrl").oninput = () => {
+        // TODO: Change playback pos based on this.
     }
 }
